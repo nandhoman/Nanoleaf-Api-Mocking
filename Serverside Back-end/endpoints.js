eb25f3f2f69f66bd18b2dbf9295b7e61ead190fc
nandhoman <55098377+nandhoman@users.noreply.github.com>
2020-04-25 23:14:18 +0200

    Create nodejs.yml

--- Serverside Back-end/endpoints.js
+++ Serverside Back-end/endpoints.js
@@ -22,74 +22,6 @@ app.use(allowCrossDomain);
 app.use(express.urlencoded());
 app.use(express.json());
 
-
-function generatecolorstring() {
-    var totalpanels;
-    var anidata = totalNumberOfPanels + " ";
-    for (totalpanels = 1; totalpanels < totalNumberOfPanels + 1; totalpanels++) {
-        var role1 = totalpanels.toString() + " ";
-        var role2 = "1 "
-        var role3 = Math.floor(Math.random() * 255).toString() + " ";
-        var role4 = Math.floor(Math.random() * 255).toString() + " ";
-        var role5 = Math.floor(Math.random() * 255).toString() + " ";
-        var role6 = Math.floor(Math.random() * 255).toString() + " ";
-        var role7 = "200 ";
-        anidata = anidata + role1 + role2 + role3 + role4 + role5 + role6 + role7;
-    }
-    return anidata
-}
-
-function random(scope) {
-    return Math.floor(Math.random() * scope).toString();
-}
-
-function firstSetMoles() {
-    var totalpanels;
-    var anidata = "103 ";
-
-    for (totalpanels = 1; totalpanels < 103; totalpanels++) {
-        var role1 = totalpanels.toString() + " ";
-        var role2 = "1 "
-        var role3 = 105 + " ";
-        var role4 = 0 + " ";
-        var role5 = 0 + " ";
-        var role6 = 0 + " ";
-        var role7 = "200 ";
-        anidata = anidata + role1 + role2 + role3 + role4 + role5 + role6 + role7;
-    }
-    var TimesMoleParTime = 1;
-    for (TimesMoleParTime = 1; TimesMoleParTime < 4; TimesMoleParTime++) {
-        var TempTileNumber = Math.floor(Math.random() * totalNumberOfPanels);
-        var OldTileRed = TempTileNumber + " 1 105 0 0 0 200";
-        var MakeTileGreen = TempTileNumber + " 1 0 255 0 0 200";
-        anidata = anidata.toString();
-        anidata = anidata.replace(OldTileRed, MakeTileGreen);
-    }
-    return anidata
-}
-
-app.post("/levelContinuation", function (request, res) {
-    console.log("function /levelContinuation")
-    var touchedTile = request.body.data;
-    var oldColors = request.body.colorstring;
-    var numberOfTouchedTile = touchedTile.replace("tile", "");
-    var TouchedTileData = numberOfTouchedTile + " 1 105 0 0 0 200";
-    var NewTouchedTileData = numberOfTouchedTile + " 1 0 255 0 0 200";
-    var oldColors = oldColors.toString();
-    var newColor = oldColors.replace(NewTouchedTileData, TouchedTileData);
-    //make new tile
-    var NewTile = random(102);
-    var NewTouchableTileGreen = NewTile + " 1 0 255 0 0 200";
-    var NewTouchableTileRed = NewTile + " 1 105 0 0 0 200";
-    var newColor = newColor.replace(NewTouchableTileRed, NewTouchableTileGreen);
-    res.send({
-        "command": "display/add",
-        "animType": "static",
-        "animData": newColor,
-        "loop": false
-    })
-});
-
 // Offical Features to FE
 var ColorstringFromFE;
 var ClickedTileArray = ["000"];
@@ -127,13 +59,6 @@ app.get("/PostNewColorString", function (req, res) {
     )
 });
 
-function range(start, end) {
-    var ans = [];
-    for (let i = start; i <= end; i++) {
-        ans.push(i);
-    }
-    return ans;
-}
 
 // Offical Endpoints
 app.get("/currentColorString", function (req, res) {
